ba95c9a3d7ccbb299cef3460a407a53fd145ff6e
Flemmli97 <Flemmli97@users.noreply.github.com>
2020-08-25 05:29:45 +0200

    water protection and piston protection (that was hell)

--- src/main/java/com/flemmli97/flan/event/WorldEvents.java
+++ src/main/java/com/flemmli97/flan/event/WorldEvents.java
@@ -3,8 +3,13 @@ package com.flemmli97.flan.event;
 import com.flemmli97.flan.claim.Claim;
 import com.flemmli97.flan.claim.ClaimStorage;
 import com.flemmli97.flan.claim.EnumPermission;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.piston.PistonBehavior;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
 
 import java.util.List;
@@ -21,5 +26,42 @@ public class WorldEvents {
                 return !claim.canInteract(null, EnumPermission.EXPLOSIONS, pos);
             return false;
         });
+    }
+
+    public static boolean pistonCanPush(BlockState state, World world, BlockPos blockPos, Direction direction, Direction pistonDir){
+        if(world.isClient||direction == Direction.UP || direction == Direction.DOWN)
+            return true;
+        boolean empty = state.isAir() || state.getPistonBehavior() == PistonBehavior.DESTROY;
+        BlockPos dirPos = blockPos.offset(direction);
+        ClaimStorage storage = ClaimStorage.get((ServerWorld) world);
+        Claim from = storage.getClaimAt(blockPos);
+        Claim to = storage.getClaimAt(dirPos);
+        boolean flag = true;
+        if(!empty){
+             if((from!=null && !from.equals(to)) || (from==null && to!=null))
+                flag = false;
+        }
+        if(from!=null && from.equals(to)){
+            Claim opp = storage.getClaimAt(blockPos.offset(direction.getOpposite()));
+            flag = from.equals(opp);
+        }
+        if(!flag) {
+            world.updateListeners(blockPos, state, state, 20);
+            BlockState toState = world.getBlockState(dirPos);
+            world.updateListeners(dirPos, toState, toState, 20);
+        }
+        return flag;
+    }
+
+    public static boolean canFlow(BlockState fluidBlockState, BlockView world, BlockPos blockPos, Direction direction){
+        if(!(world instanceof ServerWorld)||direction == Direction.UP || direction == Direction.DOWN)
+            return true;
+        ClaimStorage storage = ClaimStorage.get((ServerWorld) world);
+        Claim from = storage.getClaimAt(blockPos);
+        Claim to = storage.getClaimAt(blockPos.offset(direction));
+        boolean fl = from == null && to == null;
+        if(from!=null)
+            fl = from.equals(to);
+        return fl;
     }
 }
