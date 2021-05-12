6bc90a6f7c2210f2e3c3b5bc0835e76688c1b504
Flemmli97 <Flemmli97@users.noreply.github.com>
2021-05-12 19:58:24 +0200

    add snowman + enderman grief permission closes #31
    (cherry picked from commit 763439d907cea72a3b8b1ff6d355dc64388ec35a)

---
+++ src/main/java/com/flemmli97/flan/mixin/EndermanPlaceMixin.java
@@ -0,0 +1,27 @@
+package com.flemmli97.flan.mixin;
+
+import com.flemmli97.flan.event.EntityInteractEvents;
+import net.minecraft.block.BlockState;
+import net.minecraft.entity.mob.EndermanEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(targets = "net/minecraft/entity/mob/EndermanEntity$PlaceBlockGoal")
+public class EndermanPlaceMixin {
+
+    @Shadow
+    private EndermanEntity enderman;
+
+    @Inject(method = "Lnet/minecraft/entity/mob/EndermanEntity$PlaceBlockGoal;canPlaceOn(Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;Lnet/minecraft/block/BlockState;Lnet/minecraft/block/BlockState;Lnet/minecraft/util/math/BlockPos;)Z", at = @At(value = "HEAD"), cancellable = true)
+    private void placeCheck(World world, BlockPos posAbove, BlockState carriedState, BlockState stateAbove, BlockState state, BlockPos pos, CallbackInfoReturnable<Boolean> info) {
+        if (!EntityInteractEvents.canEndermanInteract(this.enderman, pos)) {
+            info.setReturnValue(false);
+            info.cancel();
+        }
+    }
+}
---
+++ src/main/java/com/flemmli97/flan/mixin/SnowGolemMixin.java
@@ -0,0 +1,19 @@
+package com.flemmli97.flan.mixin;
+
+import com.flemmli97.flan.event.EntityInteractEvents;
+import net.minecraft.entity.passive.SnowGolemEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(SnowGolemEntity.class)
+public abstract class SnowGolemMixin {
+
+    @Inject(method = "tickMovement", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;getGameRules()Lnet/minecraft/world/GameRules;"), cancellable = true)
+    private void checkSnow(CallbackInfo info) {
+        if (!EntityInteractEvents.canSnowGolemInteract((SnowGolemEntity) (Object) this)) {
+            info.cancel();
+        }
+    }
+}
