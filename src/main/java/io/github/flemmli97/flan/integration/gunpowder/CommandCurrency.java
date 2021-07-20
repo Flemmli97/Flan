51dbe84196e7bb91eec4ca73ebd962fe41f609ae
Flemmli97 <Flemmli97@users.noreply.github.com>
2021-07-20 19:31:08 +0200

    update gunpowder stuff

--- src/main/java/io/github/flemmli97/flan/integration/gunpowder/CommandCurrency.java
+++ src/main/java/io/github/flemmli97/flan/integration/gunpowder/CommandCurrency.java
@@ -35,6 +35,7 @@ public class CommandCurrency {
         StoredBalance bal = BalanceHandler.INSTANCE.getUser(context.getSource().getPlayer().getUuid());
         BigDecimal price = BigDecimal.valueOf(amount * ConfigHandler.config.sellPrice);
         bal.setBalance(bal.getBalance().add(price));
+        BalanceHandler.INSTANCE.updateUser(bal);
         data.setAdditionalClaims(data.getAdditionalClaims() - amount);
         context.getSource().sendFeedback(PermHelper.simpleColoredText(String.format(ConfigHandler.lang.sellSuccess, amount, price), Formatting.GOLD), false);
         return Command.SINGLE_SUCCESS;
@@ -56,6 +57,7 @@ public class CommandCurrency {
             PlayerClaimData data = PlayerClaimData.get(context.getSource().getPlayer());
             data.setAdditionalClaims(data.getAdditionalClaims() + amount);
             bal.setBalance(bal.getBalance().subtract(price));
+            BalanceHandler.INSTANCE.updateUser(bal);
             context.getSource().sendFeedback(PermHelper.simpleColoredText(String.format(ConfigHandler.lang.buySuccess, amount, price), Formatting.GOLD), false);
             return Command.SINGLE_SUCCESS;
         }
