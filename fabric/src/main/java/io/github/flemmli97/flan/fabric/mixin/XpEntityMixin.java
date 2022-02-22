f4f94404864abbf930ecc4e39300bb357abe6c42
Flemmli97 <Flemmli97@users.noreply.github.com>
2022-02-23 00:14:16 +0100

    clumps incompability fix

--- fabric/src/main/java/io/github/flemmli97/flan/fabric/mixin/XpEntityMixin.java
+++ fabric/src/main/java/io/github/flemmli97/flan/fabric/mixin/XpEntityMixin.java
@@ -8,7 +8,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(ExperienceOrbEntity.class)
+@Mixin(value = ExperienceOrbEntity.class, priority = 100)
 public abstract class XpEntityMixin {
 
     @Inject(method = "onPlayerCollision", at = @At(value = "HEAD"), cancellable = true)
