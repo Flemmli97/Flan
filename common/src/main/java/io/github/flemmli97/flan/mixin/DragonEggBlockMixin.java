a80f05a3807602b531a776c6c27335f2bde19adb
Flemmli97 <Flemmli97@users.noreply.github.com>
2023-03-10 23:42:24 +0100

    mixin thing

--- common/src/main/java/io/github/flemmli97/flan/mixin/DragonEggBlockMixin.java
+++ common/src/main/java/io/github/flemmli97/flan/mixin/DragonEggBlockMixin.java
@@ -21,7 +21,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(DragonEggBlock.class)
-public class DragonEggBlockMixin {
+public abstract class DragonEggBlockMixin {
 
     @Unique
     private Player flanTempPlayer;
