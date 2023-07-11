71029f2d25859ee3c7cea45674b86ccce5d24fa0
Flemmli97 <Flemmli97@users.noreply.github.com>
2023-07-12 00:21:12 +0200

    add option to autoclaim structures close #212

---
+++ common/src/main/java/io/github/flemmli97/flan/mixin/ChunkGeneratorMixin.java
@@ -0,0 +1,21 @@
+package io.github.flemmli97.flan.mixin;
+
+import io.github.flemmli97.flan.event.WorldEvents;
+import net.minecraft.world.level.StructureManager;
+import net.minecraft.world.level.chunk.ChunkGenerator;
+import net.minecraft.world.level.levelgen.structure.StructureSet;
+import net.minecraft.world.level.levelgen.structure.StructureStart;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+
+@Mixin(ChunkGenerator.class)
+public class ChunkGeneratorMixin {
+
+    @ModifyVariable(method = "tryGenerateStructure", at = @At(value = "INVOKE",
+            target = "Lnet/minecraft/world/level/StructureManager;setStartForStructure(Lnet/minecraft/core/SectionPos;Lnet/minecraft/world/level/levelgen/structure/Structure;Lnet/minecraft/world/level/levelgen/structure/StructureStart;Lnet/minecraft/world/level/chunk/StructureAccess;)V"))
+    private StructureStart onStructureGenerate(StructureStart origin, StructureSet.StructureSelectionEntry structureSelectionEntry, StructureManager structureManager) {
+        WorldEvents.onStructureGen(origin, structureManager);
+        return origin;
+    }
+}
---
+++ common/src/main/java/io/github/flemmli97/flan/mixin/StructureManagerAccessor.java
@@ -0,0 +1,13 @@
+package io.github.flemmli97.flan.mixin;
+
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.StructureManager;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(StructureManager.class)
+public interface StructureManagerAccessor {
+
+    @Accessor("level")
+    LevelAccessor getLevel();
+}
