4e9744b71d05092539cdfbd24f3ead866da37d2c
Flemmli97 <Flemmli97@users.noreply.github.com>
2023-07-12 16:46:27 +0200

    only prevent natural spawn fix #234

--- forge/src/main/java/io/github/flemmli97/flan/forge/forgeevent/WorldEventsForge.java
+++ forge/src/main/java/io/github/flemmli97/flan/forge/forgeevent/WorldEventsForge.java
@@ -3,6 +3,7 @@ package io.github.flemmli97.flan.forge.forgeevent;
 import io.github.flemmli97.flan.event.WorldEvents;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.Mob;
+import net.minecraft.world.entity.MobSpawnType;
 import net.minecraftforge.event.entity.living.LivingSpawnEvent;
 import net.minecraftforge.event.world.ExplosionEvent;
 import net.minecraftforge.eventbus.api.Event;
@@ -15,7 +16,7 @@ public class WorldEventsForge {
     }
 
     public static void preventMobSpawn(LivingSpawnEvent.CheckSpawn event) {
-        if (!(event.getWorld() instanceof ServerLevel) || !(event.getEntityLiving() instanceof Mob))
+        if (!(event.getWorld() instanceof ServerLevel) || !(event.getEntityLiving() instanceof Mob) || event.getSpawnReason() != MobSpawnType.NATURAL)
             return;
         if (WorldEvents.preventMobSpawn((ServerLevel) event.getWorld(), (Mob) event.getEntityLiving()))
             event.setResult(Event.Result.DENY);
