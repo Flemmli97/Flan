05bffc83840d80753666345c5c25a700e6538f92
Flemmli97 <Flemmli97@users.noreply.github.com>
2023-07-16 12:46:14 +0200

    fix structure config not working

--- common/src/main/java/io/github/flemmli97/flan/event/WorldEvents.java
+++ common/src/main/java/io/github/flemmli97/flan/event/WorldEvents.java
@@ -3,6 +3,7 @@ package io.github.flemmli97.flan.event;
 import io.github.flemmli97.flan.api.data.IPermissionContainer;
 import io.github.flemmli97.flan.api.permission.PermissionRegistry;
 import io.github.flemmli97.flan.claim.ClaimStorage;
+import io.github.flemmli97.flan.config.ConfigHandler;
 import io.github.flemmli97.flan.mixin.StructureManagerAccessor;
 import io.github.flemmli97.flan.player.LogoutTracker;
 import net.minecraft.core.BlockPos;
@@ -106,6 +107,8 @@ public class WorldEvents {
 
     @SuppressWarnings("deprecation")
     public static void onStructureGen(StructureStart structureStart, StructureFeatureManager structureManager) {
+        if (!ConfigHandler.config.autoClaimStructures)
+            return;
         LevelAccessor acc = ((StructureManagerAccessor) structureManager).getLevel();
         ServerLevel level = null;
         if (acc instanceof WorldGenRegion region)
