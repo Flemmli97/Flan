0c8b883d61d833c0750279203a7906b3eee669c8
Flemmli97 <Flemmli97@users.noreply.github.com>
2025-01-10 22:09:59 +0100

    fix patrol mixin

--- fabric/src/main/java/io/github/flemmli97/flan/fabric/mixin/PatrolSpawnerMixin.java
+++ fabric/src/main/java/io/github/flemmli97/flan/fabric/mixin/PatrolSpawnerMixin.java
@@ -4,11 +4,14 @@ import io.github.flemmli97.flan.event.WorldEvents;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.monster.PatrollingMonster;
+import net.minecraft.world.entity.monster.Phantom;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.levelgen.PatrolSpawner;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
@@ -17,10 +20,18 @@ import java.util.Random;
 @Mixin(PatrolSpawner.class)
 public class PatrolSpawnerMixin {
 
-    @Inject(method = "spawnPatrolMember", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/monster/PatrollingMonster;finalizeSpawn(Lnet/minecraft/world/level/ServerLevelAccessor;Lnet/minecraft/world/DifficultyInstance;Lnet/minecraft/world/entity/MobSpawnType;Lnet/minecraft/world/entity/SpawnGroupData;Lnet/minecraft/nbt/CompoundTag;)Lnet/minecraft/world/entity/SpawnGroupData;"),
-            locals = LocalCapture.CAPTURE_FAILHARD, cancellable = true)
-    private void patrolSpawnCheck(ServerLevel level, BlockPos pos, Random random, boolean leader, CallbackInfoReturnable<Boolean> cir, BlockState blockState, PatrollingMonster patrollingMonster) {
-        if (WorldEvents.preventMobSpawn(level, patrollingMonster))
-            cir.setReturnValue(false);
+    @Unique
+    private PatrollingMonster flan_patrol_var;
+
+    @ModifyVariable(method = "spawnPatrolMember", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/monster/PatrollingMonster;finalizeSpawn(Lnet/minecraft/world/level/ServerLevelAccessor;Lnet/minecraft/world/DifficultyInstance;Lnet/minecraft/world/entity/MobSpawnType;Lnet/minecraft/world/entity/SpawnGroupData;Lnet/minecraft/nbt/CompoundTag;)Lnet/minecraft/world/entity/SpawnGroupData;"))
+    private PatrollingMonster phantomSpawnCheckPrepare(PatrollingMonster monster) {
+        this.flan_patrol_var = monster;
+        return monster;
+    }
+    
+    @Inject(method = "spawnPatrolMember", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/monster/PatrollingMonster;finalizeSpawn(Lnet/minecraft/world/level/ServerLevelAccessor;Lnet/minecraft/world/DifficultyInstance;Lnet/minecraft/world/entity/MobSpawnType;Lnet/minecraft/world/entity/SpawnGroupData;Lnet/minecraft/nbt/CompoundTag;)Lnet/minecraft/world/entity/SpawnGroupData;"), cancellable = true)
+    private void patrolSpawnCheck(ServerLevel level, BlockPos pos, Random random, boolean leader, CallbackInfoReturnable<Boolean> info) {
+        if (this.flan_patrol_var != null && WorldEvents.preventMobSpawn(level, this.flan_patrol_var))
+            info.setReturnValue(false);
     }
 }
