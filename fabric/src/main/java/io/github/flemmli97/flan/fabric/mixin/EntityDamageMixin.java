3cee1e713ee14f34dc092abc37423d148b95eb3c
Flemmli97 <Flemmli97@users.noreply.github.com>
2025-07-16 14:17:20 +0200

    fix #394 damage check reversed

--- fabric/src/main/java/io/github/flemmli97/flan/fabric/mixin/EntityDamageMixin.java
+++ fabric/src/main/java/io/github/flemmli97/flan/fabric/mixin/EntityDamageMixin.java
@@ -8,13 +8,13 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(Entity.class)
+@Mixin(value = Entity.class, priority = 999)
 public abstract class EntityDamageMixin {
 
     @Inject(method = "isInvulnerableTo", at = @At(value = "HEAD"), cancellable = true)
     private void onDamage(DamageSource source, CallbackInfoReturnable<Boolean> info) {
         if (EntityInteractEvents.preventDamage((Entity) (Object) this, source)) {
-            info.setReturnValue(false);
+            info.setReturnValue(true);
             info.cancel();
         }
     }
