aedb7cdd529af5f6be36e03397108a033a7d5a71
Flemmli97 <Flemmli97@users.noreply.github.com>
2025-07-28 18:13:59 +0200

    fix permissions directory change

--- neoforge/src/main/java/io/github/flemmli97/flan/neoforge/FlanNeoForge.java
+++ neoforge/src/main/java/io/github/flemmli97/flan/neoforge/FlanNeoForge.java
@@ -66,7 +66,7 @@ public class FlanNeoForge {
     }
 
     public void addReloadListener(AddServerReloadListenersEvent event) {
-        event.addListener(PermissionManager.ID.location(), PermissionManager.create(event.getRegistryAccess()));
-        event.addListener(InteractionOverrideManager.ID.location(), InteractionOverrideManager.create(event.getRegistryAccess()));
+        event.addListener(PermissionManager.ID.location(), PermissionManager.create(event.getServerResources().getRegistryLookup()));
+        event.addListener(InteractionOverrideManager.ID.location(), InteractionOverrideManager.create(event.getServerResources().getRegistryLookup()));
     }
 }
