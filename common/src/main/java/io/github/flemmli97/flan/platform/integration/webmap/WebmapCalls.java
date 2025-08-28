eddd00cb56c60facaab726728f54fbeb639b07da
Flemmli97 <Flemmli97@users.noreply.github.com>
2025-08-28 10:39:13 +0200

    add config to disable webmap integrations

--- common/src/main/java/io/github/flemmli97/flan/platform/integration/webmap/WebmapCalls.java
+++ common/src/main/java/io/github/flemmli97/flan/platform/integration/webmap/WebmapCalls.java
@@ -1,6 +1,7 @@
 package io.github.flemmli97.flan.platform.integration.webmap;
 
 import io.github.flemmli97.flan.claim.Claim;
+import io.github.flemmli97.flan.config.ConfigHandler;
 
 public class WebmapCalls {
 
@@ -8,39 +9,39 @@ public class WebmapCalls {
     public static boolean bluemapLoaded;
 
     public static void addClaimMarker(Claim claim) {
-        if (dynmapLoaded)
+        if (dynmapLoaded && ConfigHandler.CONFIG.dynmapIntegration)
             DynmapIntegration.addClaimMarker(claim);
-        if (bluemapLoaded)
+        if (bluemapLoaded && ConfigHandler.CONFIG.bluemapIntegration)
             BluemapIntegration.addClaimMarker(claim);
     }
 
     public static void removeMarker(Claim claim) {
-        if (dynmapLoaded)
+        if (dynmapLoaded && ConfigHandler.CONFIG.dynmapIntegration)
             DynmapIntegration.removeMarker(claim);
-        if (bluemapLoaded)
+        if (bluemapLoaded && ConfigHandler.CONFIG.bluemapIntegration)
             BluemapIntegration.removeMarker(claim);
     }
 
     public static void changeClaimName(Claim claim) {
-        if (dynmapLoaded)
+        if (dynmapLoaded && ConfigHandler.CONFIG.dynmapIntegration)
             DynmapIntegration.changeClaimName(claim);
-        if (bluemapLoaded)
+        if (bluemapLoaded && ConfigHandler.CONFIG.bluemapIntegration)
             BluemapIntegration.changeClaimName(claim);
     }
 
     public static void changeClaimOwner(Claim claim) {
-        if (dynmapLoaded)
+        if (dynmapLoaded && ConfigHandler.CONFIG.dynmapIntegration)
             DynmapIntegration.changeClaimOwner(claim);
-        if (bluemapLoaded)
+        if (bluemapLoaded && ConfigHandler.CONFIG.bluemapIntegration)
             BluemapIntegration.changeClaimOwner(claim);
     }
 
     public static void onExtendDownwards(Claim claim) {
-        if (dynmapLoaded) {
+        if (dynmapLoaded && ConfigHandler.CONFIG.dynmapIntegration) {
             DynmapIntegration.removeMarker(claim);
             DynmapIntegration.addClaimMarker(claim);
         }
-        if (bluemapLoaded)
+        if (bluemapLoaded && ConfigHandler.CONFIG.bluemapIntegration)
             BluemapIntegration.addClaimMarker(claim);
     }
 }
