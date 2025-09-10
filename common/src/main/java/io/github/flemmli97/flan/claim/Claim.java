Flemmli97/Flan

commit 374c80fbb5202770acae36aa7b7b0de3e23e9743

add `require_explicit` closing #400

common/src/main/java/io/github/flemmli97/flan/claim/Claim.java
@@ -8,6 +8,7 @@ import com.google.gson.JsonObject;
 import com.mojang.authlib.GameProfile;
 import io.github.flemmli97.flan.api.data.IPermissionContainer;
 import io.github.flemmli97.flan.api.permission.BuiltinPermission;
+import io.github.flemmli97.flan.api.permission.ClaimPermission;
 import io.github.flemmli97.flan.api.permission.PermissionManager;
 import io.github.flemmli97.flan.config.Config;
 import io.github.flemmli97.flan.config.ConfigHandler;
@@ -324,7 +325,7 @@ public class Claim implements IPermissionContainer {
         if (!this.isAdminClaim()) {
             Config.GlobalType global = ConfigHandler.CONFIG.getGlobal(this.level, perm);
             if (!global.canModify()) {
-                if (global.getValue() || (player != null && this.isAdminIgnore(player)))
+                if (global.getValue() || (player != null && this.playerBypassesPermission(player, perm)))
                     return true;
                 if (message)
                     player.displayClientMessage(ClaimUtils.translatedText("flan.noPermissionSimple", ChatFormatting.DARK_RED), true);
@@ -350,7 +351,7 @@ public class Claim implements IPermissionContainer {
                 this.getOwnerPlayer().ifPresent(p -> PlayerClaimData.get(p).notifyFakePlayerInteraction(player, pos, this));
             return false;
         }
-        if (this.isAdminIgnore(player) || player.getUUID().equals(this.owner))
+        if (this.playerBypassesPermission(player, perm))
             return true;
         if (!perm.equals(BuiltinPermission.EDITCLAIM) && !perm.equals(BuiltinPermission.EDITPERMS))
             for (Claim claim : this.subClaims) {
@@ -379,8 +380,13 @@ public class Claim implements IPermissionContainer {
         return false;
     }
 
-    private boolean isAdminIgnore(ServerPlayer player) {
+    private boolean playerBypassesPermission(ServerPlayer player, ResourceLocation perm) {
         if (player == null)
+            return true;
+        ClaimPermission permission = PermissionManager.getInstance().get(perm);
+        if (permission != null && permission.requireExplicitSet)
+            return false;
+        if (player.getUUID().equals(this.owner))
             return true;
         if (PlayerClaimData.get(player).isAdminIgnoreClaim())
             return !this.isAdminClaim() || PermissionNodeHandler.INSTANCE.perm(player, PermissionNodeHandler.ADMIN_BYPASS, true);

common/src/main/java/io/github/flemmli97/flan/api/data/IPermissionContainer.java
@@ -16,5 +16,4 @@ public interface IPermissionContainer {
      * @param player The player doing the action. Can be null
      */
     boolean canInteract(ServerPlayer player, ResourceLocation perm, BlockPos pos, boolean message);
-
 }

common/src/main/java/io/github/flemmli97/flan/api/permission/BuiltinPermission.java
@@ -14,6 +14,7 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
+import java.util.function.BiFunction;
 import java.util.function.Function;
 
 /**
@@ -75,7 +76,7 @@ public class BuiltinPermission {
     public static final ResourceLocation DROP = register("drop", new ItemStack(Items.BOWL), true, "Allow the drop of items");
     public static final ResourceLocation PICKUP = register("pickup", new ItemStack(Items.BRICK), true, "Allow the pickup of items");
     public static final ResourceLocation ALLOW_FLIGHT = register("flight", new ItemStack(Items.IRON_BLOCK), true, "Allows all non creative flight", "Does not grant flight!");
-    public static final ResourceLocation MAY_FLIGHT = register("may_flight", new ItemStack(Items.FEATHER), false, "Allows player to fly in this claim.", "Flight permission needs to be true!");
+    public static final ResourceLocation MAY_FLIGHT = register("may_flight", (holder, order) -> new ClaimPermission.Builder(new ItemStack(Items.FEATHER), order, List.of("Allows player to fly in this claim.", "Flight permission needs to be true!")).requireExplicitSet(true));
     public static final ResourceLocation CANSTAY = register("can_stay", new ItemStack(Items.PAPER), true, "Allow players to enter your claim");
     public static final ResourceLocation TELEPORT = register("teleport", new ItemStack(Items.END_PORTAL_FRAME), false, "Allow player to teleport to your claim home position");
     public static final ResourceLocation NOHUNGER = register("no_hunger", new ItemStack(Items.COOKED_BEEF), false, "Disable hunger");
@@ -112,9 +113,13 @@ public class BuiltinPermission {
     }
 
     private static ResourceLocation register(String key, Function<HolderLookup.Provider, ItemStack> item, boolean defaultVal, boolean global, String... description) {
+        return register(key, (holder, order) -> new ClaimPermission.Builder(item.apply(holder), order, List.of(description)).defaultVal(defaultVal).global(global));
+    }
+
+    private static ResourceLocation register(String key, BiFunction<HolderLookup.Provider, Integer, ClaimPermission.Builder> builder) {
         ResourceLocation id = ResourceLocation.fromNamespaceAndPath(Flan.MODID, key);
         if (CrossPlatformStuff.INSTANCE.isDataGen()) {
-            DATAGEN_DATA.put(id, holder -> new ClaimPermission.Builder(item.apply(holder), defaultVal, global, order++, List.of(description)));
+            DATAGEN_DATA.put(id, holder -> builder.apply(holder, order++));
         }
         LEGACY_MIGRATION.put(key.replace("_", "").toUpperCase(Locale.ROOT), id);
         return id;

common/src/main/java/io/github/flemmli97/flan/api/permission/ClaimPermission.java
@@ -32,13 +32,19 @@ public class ClaimPermission implements Comparable<ClaimPermission> {
      */
     public final boolean global;
     /**
+     * Whether this permission needs to have its value set in the claim to function
+     * If this is true admin and owner will be treated as normal players (aka the claims permission needs to be true to apply to them)
+     */
+    public final boolean requireExplicitSet;
+    /**
      * The order in which this permission appears in the gui. If a permission has the same ordering the id will determine the order
      */
     public final int order;
 
-    private ClaimPermission(ResourceLocation id, ItemStack guiItem, boolean defaultVal, boolean global, int order) {
+    private ClaimPermission(ResourceLocation id, ItemStack guiItem, boolean defaultVal, boolean global, boolean requireExplicitSet, int order) {
         this.id = id;
         this.guiItem = guiItem;
+        this.requireExplicitSet = requireExplicitSet;
         this.order = order;
         this.defaultVal = defaultVal;
         this.global = global;
@@ -90,43 +96,60 @@ public class ClaimPermission implements Comparable<ClaimPermission> {
     public static class Builder {
 
         public static final Codec<ClaimPermission.Builder> CODEC = RecordCodecBuilder.create((instance) ->
-                instance.group(
-                        Codec.INT.fieldOf("order").forGetter(d -> d.order),
-                        Codec.STRING.optionalFieldOf("required_mod").forGetter(d -> Optional.ofNullable(d.requiredMod)),
-
-                        ItemStackHolder.CODEC.fieldOf("gui_item").forGetter(d -> d.guiItem),
+                instance.group(ItemStackHolder.CODEC.fieldOf("gui_item").forGetter(d -> d.guiItem),
                         Codec.BOOL.fieldOf("default_value").forGetter(d -> d.defaultVal),
-                        Codec.BOOL.fieldOf("global").forGetter(d -> d.global)
-                ).apply(instance, (order, requiredMod, item, val, global) -> new ClaimPermission.Builder(item, val, global, order, requiredMod.orElse(null), null)));
+                        Codec.BOOL.fieldOf("global").forGetter(d -> d.global),
+                        Codec.BOOL.optionalFieldOf("require_explicit").forGetter(d -> !d.requireExplicitSet ? Optional.empty() : Optional.of(true)),
+                        Codec.INT.fieldOf("order").forGetter(d -> d.order),
+                        Codec.STRING.optionalFieldOf("required_mod").forGetter(d -> Optional.ofNullable(d.requiredMod))
+                ).apply(instance, (item, val, global, explicit, order, requiredMod)
+                        -> new ClaimPermission.Builder(item, order, null).defaultVal(val).global(global).requireExplicitSet(explicit.orElse(false)).requiredMod(requiredMod.orElse(null))));
 
         private final ItemStackHolder guiItem;
-        // Only used for datagen
+        /**
+         * Only used for datagen
+         */
         public final List<String> desc;
-        private final boolean defaultVal;
-        private final boolean global;
-        private final String requiredMod;
+        private boolean defaultVal;
+        private boolean global;
+        private String requiredMod;
 
         private final int order;
 
-        public Builder(ItemStack guiItem, boolean defaultVal, boolean global, int order, List<String> desc) {
-            this(new ItemStackHolder(guiItem), defaultVal, global, order, desc);
-        }
+        private boolean requireExplicitSet;
 
-        public Builder(ItemStackHolder guiItem, boolean defaultVal, boolean global, int order, List<String> desc) {
-            this(guiItem, defaultVal, global, order, null, desc);
+        public Builder(ItemStack guiItem, int order, List<String> desc) {
+            this(new ItemStackHolder(guiItem), order, desc);
         }
 
         /**
          * The builder for a claim permission used in datagen. And reloading.
          * See Claimpermission fields what the fields stand for
          */
-        public Builder(ItemStackHolder guiItem, boolean defaultVal, boolean global, int order, String requiredMod, List<String> desc) {
+        public Builder(ItemStackHolder guiItem, int order, List<String> desc) {
             this.guiItem = guiItem;
             this.desc = desc;
-            this.defaultVal = defaultVal;
-            this.global = global;
             this.order = order;
+        }
+
+        public Builder defaultVal(boolean defaultVal) {
+            this.defaultVal = defaultVal;
+            return this;
+        }
+
+        public Builder global(boolean global) {
+            this.global = global;
+            return this;
+        }
+
+        public Builder requireExplicitSet(boolean requireExplicitSet) {
+            this.requireExplicitSet = requireExplicitSet;
+            return this;
+        }
+
+        public Builder requiredMod(String requiredMod) {
             this.requiredMod = requiredMod;
+            return this;
         }
 
         public boolean verify() {
@@ -134,7 +157,7 @@ public class ClaimPermission implements Comparable<ClaimPermission> {
         }
 
         public ClaimPermission build(ResourceLocation id) {
-            return new ClaimPermission(id, this.guiItem.toStack(), this.defaultVal, this.global, this.order);
+            return new ClaimPermission(id, this.guiItem.toStack(), this.defaultVal, this.global, this.requireExplicitSet, this.order);
         }
 
         public record ItemStackHolder(ResourceLocation item, int count, DataComponentPatch components) {

common/src/main/java/io/github/flemmli97/flan/claim/ClaimBox.java
@@ -1,6 +1,5 @@
 package io.github.flemmli97.flan.claim;
 
-
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.phys.AABB;
 

common/src/main/java/io/github/flemmli97/flan/claim/GlobalClaim.java
@@ -2,6 +2,8 @@ package io.github.flemmli97.flan.claim;
 
 import io.github.flemmli97.flan.api.data.IPermissionContainer;
 import io.github.flemmli97.flan.api.permission.BuiltinPermission;
+import io.github.flemmli97.flan.api.permission.ClaimPermission;
+import io.github.flemmli97.flan.api.permission.PermissionManager;
 import io.github.flemmli97.flan.config.Config;
 import io.github.flemmli97.flan.config.ConfigHandler;
 import io.github.flemmli97.flan.player.PlayerClaimData;
@@ -17,7 +19,7 @@ public record GlobalClaim(ServerLevel world) implements IPermissionContainer {
     public boolean canInteract(ServerPlayer player, ResourceLocation perm, BlockPos pos, boolean message) {
         message = message && player.getClass().equals(ServerPlayer.class); //dont send messages to fake players
         Config.GlobalType global = ConfigHandler.CONFIG.getGlobal(this.world, perm);
-        if (global != Config.GlobalType.NONE && (player == null || !PlayerClaimData.get(player).isAdminIgnoreClaim())) {
+        if (global != Config.GlobalType.NONE && (player == null || !this.isAdmin(player, perm))) {
             if (global.getValue())
                 return true;
             if (message)
@@ -25,5 +27,12 @@ public record GlobalClaim(ServerLevel world) implements IPermissionContainer {
             return false;
         }
         return !perm.equals(BuiltinPermission.MOBSPAWN) && !perm.equals(BuiltinPermission.ANIMALSPAWN);
+    }
+
+    private boolean isAdmin(ServerPlayer player, ResourceLocation perm) {
+        ClaimPermission permission = PermissionManager.getInstance().get(perm);
+        if (permission != null && permission.requireExplicitSet)
+            return false;
+        return PlayerClaimData.get(player).isAdminIgnoreClaim();
     }
 }

common/src/main/java/io/github/flemmli97/flan/config/BuySellHandler.java
@@ -306,6 +306,7 @@ public class BuySellHandler {
     }
 
     record BuyItem(float amount, ItemPredicate predicate) implements Comparable<BuyItem> {
+
         @Override
         public int compareTo(@NotNull BuyItem buyItem) {
             return Float.compare(buyItem.amount, this.amount);
@@ -313,6 +314,7 @@ public class BuySellHandler {
     }
 
     record SellItem(float amount, ItemStack item) implements Comparable<SellItem> {
+
         @Override
         public int compareTo(@NotNull SellItem item) {
             return Float.compare(item.amount, this.amount);
@@ -320,5 +322,6 @@ public class BuySellHandler {
     }
 
     record ItemResult(ItemStack stack, int amount, float value) {
+
     }
 }

common/src/main/java/io/github/flemmli97/flan/gui/ConfirmScreenHandler.java
@@ -38,7 +38,6 @@ public class ConfirmScreenHandler extends ServerOnlyScreenHandler<Object> {
         player.openMenu(fac);
     }
 
-
     @Override
     protected void fillInventoryWith() {
         for (int i = 0; i < this.inventory.getContainerSize(); i++) {

commit 1cf2be69e4c419facae86dfecd1f24eb6867c136

fix #415, fix #413 due to regression from 374c80fb

common/src/main/java/io/github/flemmli97/flan/claim/Claim.java
@@ -323,7 +323,7 @@ public class Claim implements IPermissionContainer {
         if (!this.isAdminClaim()) {
             Config.GlobalType global = ConfigHandler.CONFIG.getGlobal(this.level, perm);
             if (!global.canModify()) {
-                if (global.getValue() || (player != null && this.playerBypassesPermission(player, perm)))
+                if (global.getValue() || (player != null && this.isAdminIgnore(player)))
                     return true;
                 if (message)
                     player.displayClientMessage(ClaimUtils.translatedText("flan.noPermissionSimple", ChatFormatting.DARK_RED), true);
@@ -384,7 +384,11 @@ public class Claim implements IPermissionContainer {
         ClaimPermission permission = PermissionManager.getInstance().get(perm);
         if (permission != null && permission.requireExplicitSet)
             return false;
-        if (player.getUUID().equals(this.owner))
+        return player.getUUID().equals(this.owner) || this.isAdminIgnore(player);
+    }
+
+    private boolean isAdminIgnore(ServerPlayer player) {
+        if (player == null)
             return true;
         if (PlayerClaimData.get(player).isAdminIgnoreClaim())
             return !this.isAdminClaim() || PermissionNodeHandler.INSTANCE.perm(player, PermissionNodeHandler.ADMIN_BYPASS, true);