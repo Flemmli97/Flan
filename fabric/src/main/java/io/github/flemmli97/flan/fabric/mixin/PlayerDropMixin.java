7ed856f0a9e1b8baab67a667e80e5f06e03c91e4
zonary123 <carlosvarasalonso10@gmail.com>
2025-10-21 09:25:06 +0200

    Removed LOGGER.

--- fabric/src/main/java/io/github/flemmli97/flan/fabric/mixin/PlayerDropMixin.java
+++ fabric/src/main/java/io/github/flemmli97/flan/fabric/mixin/PlayerDropMixin.java
@@ -4,8 +4,6 @@ import io.github.flemmli97.flan.event.PlayerEvents;
 import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
@@ -14,9 +12,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(value = Player.class)
 public abstract class PlayerDropMixin {
-
-    @Unique private static final Logger LOGGER = LoggerFactory.getLogger("PlayerDropMixin");
-
     @Inject(
         method = "drop(Lnet/minecraft/world/item/ItemStack;Z)Lnet/minecraft/world/entity/item/ItemEntity;",
         at = @At("HEAD"),
@@ -41,10 +36,6 @@ public abstract class PlayerDropMixin {
         Player player = (Player) (Object) this;
 
         if (!PlayerEvents.canDropItem(player, stack)) {
-            LOGGER.info("[FLAN] Drop blocked for player {}. Item: {} x{}",
-                player.getName().getString(),
-                stack.getItem().toString(),
-                stack.getCount());
 
             // Cancel the drop entirely; no need to restore item manually
             var inventory = player.getInventory();
