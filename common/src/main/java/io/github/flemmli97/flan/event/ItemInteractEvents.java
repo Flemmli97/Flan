454712dd670988b89fbb76651bf89f8fea5c5ca0
Wesley1808 <74668381+Wesley1808@users.noreply.github.com>
2025-12-27 11:51:19 +0100

    Remove updateHeldItem magic number comment

--- common/src/main/java/io/github/flemmli97/flan/event/ItemInteractEvents.java
+++ common/src/main/java/io/github/flemmli97/flan/event/ItemInteractEvents.java
@@ -141,10 +141,7 @@ public class ItemInteractEvents {
         updateHeldItem(player);
         return InteractionResult.FAIL;
     }
-
-    /**
-     * -2 == Main inventory update
-     */
+    
     private static void updateHeldItem(ServerPlayer player) {
         player.connection.send(new ClientboundSetPlayerInventoryPacket(player.getInventory().getSelectedSlot(), player.getInventory().getSelectedItem()));
         player.connection.send(new ClientboundSetPlayerInventoryPacket(Inventory.SLOT_OFFHAND, player.getInventory().getItem(Inventory.SLOT_OFFHAND)));
